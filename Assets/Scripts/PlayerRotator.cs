15f065ed62ad219e75805aca0eb7fb2c3e207f4d
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-02-04 20:06:11 +0300

    Add simple movement and weapon rotation.

---
+++ Assets/Scripts/PlayerRotator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerRotator : MonoBehaviour
+    {
+        [SerializeField] private float _mouseSensitivity = 10f;
+        [SerializeField] private Transform _playerBody;
+
+        private float _xAxis = 0f;
+
+        private void Start()
+        {
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true; 
+        }
+
+        private void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+
+            _xAxis -= mouseY;
+            _xAxis = Mathf.Clamp(_xAxis, -90f, 90f); 
+            
+            transform.localRotation = Quaternion.Euler(_xAxis, 0f, 0f);
+            _playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
