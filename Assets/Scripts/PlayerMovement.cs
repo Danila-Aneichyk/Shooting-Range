be6462b4e586a9488edfd64db4a20316019454ae
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-02-06 21:19:48 +0300

    Recreate movement of player on Character Controller.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Base Settings")]
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private float _gravityMultiplier = 1f;
     [SerializeField] private float _speed = 3f;
 
+    [Header("Ground Settings")]
+    [SerializeField] private Transform _checkGroundTransform;
+    [SerializeField] private float _checkGroundRadius;
+    [SerializeField] private LayerMask _checkGroundMask;
+    
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 moveVector = transform.right * horizontal + transform.forward * vertical;
-        Vector3 position = transform.position; 
-        position += moveVector * (_speed * Time.deltaTime);
-        transform.position = position; 
+        moveVector *= (_speed * Time.deltaTime);
+        _characterController.Move(moveVector);
+
+
+        bool isGrounded = Physics.CheckSphere(_checkGroundTransform.position, _checkGroundRadius, _checkGroundMask);
+        Debug.Log(isGrounded);
+        
+        Vector3 fallVector = Vector3.zero;
+        float gravity = Physics.gravity.y * _gravityMultiplier;
+        fallVector.y += gravity * Time.deltaTime;
+
+        _characterController.Move(fallVector);
     }
 }
